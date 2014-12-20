365a931fd8c883626154f281fbaf14ca7efd25f6
discully <daniel.i.scully@gmail.com>
2014-12-20 10:16:44 +0000

    document constants, and add a new one (BOARD_ROW)

--- src/Rules.hxx
+++ src/Rules.hxx
@@ -19,13 +19,29 @@ namespace zebra
 class zebra::Rules
 {
 	public:
-	
+		
+		/// The number of squares in a row/column of the board.
 		static const unsigned int BOARD_SIZE  = 8;
-		static const unsigned int BOARD_SQUARES = (BOARD_SIZE / 2) * BOARD_SIZE;
+		
+		/// The number of playable square in a row/column of the board.
+		static const unsigned int BOARD_ROW = BOARD_SIZE / 2;
+		
+		/// The total number of playable squares on the board.
+		static const unsigned int BOARD_SQUARES = BOARD_ROW * BOARD_SIZE;
+		
+		/// The number of pieces a player has at the start of a game.
 		static const unsigned int PLAYER_PIECES = 3 * (BOARD_SIZE / 2);
+		
+		/// The change in square number resulting from a slide in the NW or SE direction.
 		static const unsigned int SLIDE_SHORT = 3;
+		
+		/// The change in square number resulting from a slide in the NE or SW direction.
 		static const unsigned int SLIDE_LONG  = 4;
+		
+		/// The change in square number resulting from a jump in the NW or SE direction.
 		static const unsigned int JUMP_SHORT  = 7;
+		
+		/// The change in square number resulting from a jump in the NE or SW direction.
 		static const unsigned int JUMP_LONG   = 9;
 };
 
