4e965e04b010ea3f40b84868a376882f80e3df3a
discully <daniel.i.scully@gmail.com>
2014-12-24 21:24:03 +0000

    add logging capability to game

--- src/Game.cxx
+++ src/Game.cxx
@@ -1,23 +1,60 @@
 #include "Game.hxx"
 
+#include "Player.hxx"
 
 
-zebra::Game::Game()
-	: state(), black(nullptr), next(&black), white(nullptr)
+
+zebra::Game::Game(bool logging)
+	: state(),
+	black(nullptr),
+	next(&black),
+	white(nullptr),
+	history( logging ? new std::vector<std::vector<zebra::Move>>() : nullptr )
 {}
 
 
 
-zebra::Game::Game(zebra::Player* black_player, zebra::Player* white_player)
-	: state(), black(black_player), next(&black), white(white_player)
-{}
+zebra::Game::Game(zebra::Player* black_player, zebra::Player* white_player, bool logging)
+	: state(),
+	black(black_player),
+	next(&black),
+	white(white_player),
+	history( logging ? new std::vector<std::vector<zebra::Move>>() : nullptr )
+{
+	this->black->play( this, true );
+	this->white->play( this, false );
+}
 
 
 
 zebra::Game::Game(const zebra::Game& g)
-	: state(g.state), black(nullptr), next(nullptr), white(nullptr)
+	: state(g.state),
+	black(nullptr),
+	next( (g.next == &(g.black)) ? &(black) : &(white) ),
+	white(nullptr),
+	history( g.logging() ? new std::vector<std::vector<zebra::Move>>(*(g.history)) : nullptr)
+{}
+
+
+
+zebra::Game::~Game()
 {
-	this->next = ( g.next == &(g.black) ) ? &(this->black) : &(this->white);
+	if( this->history != nullptr )
+	{
+		delete(this->history);
+	}
+	
+	if( (this->black != nullptr) && (this->black->game() == this) )
+	{
+		this->black->play(nullptr, true);
+		this->black = nullptr;
+	}
+	
+	if( (this->white != nullptr) && (this->white->game() == this) )
+	{
+		this->white->play(nullptr, false);
+		this->white = nullptr;
+	}
 }
 
 
@@ -25,6 +62,34 @@ zebra::Game::Game(const zebra::Game& g)
 const zebra::Board& zebra::Game::board() const
 {
 	return this->state;
+}
+
+
+
+std::vector< std::vector<zebra::Move> > zebra::Game::log() const
+{
+	if( this->history == nullptr )
+	{
+		return std::vector< std::vector<Move> >();
+	}
+	else
+	{
+		return *(this->history);
+	}
+}
+
+
+
+bool zebra::Game::logging() const
+{
+	return (this->history == nullptr);
+}
+
+
+
+void zebra::Game::logTurn(const std::vector<Move>& turn)
+{
+	this->history->push_back( turn );
 }
 
 
--- src/Game.hxx
+++ src/Game.hxx
@@ -19,22 +19,44 @@ class zebra::Game
 {
 	public:
 		
-		Game();
+		/// Constructs a new game with no players.
+		/// \param logging \c true (default) to enable logging, \c false to disable logging.
+		explicit Game(bool logging = true);
 		
 		/// Constructs a new game with specified players
-		Game(Player* black_player, Player* white_player);
+		/// \param black_player Pointer to an instance of a player to be black.
+		/// \param white_player Pointer to an instance of a player to be white.
+		/// \param logging \c true (default) to enable logging, \c false to disable logging.
+		Game(Player* black_player, Player* white_player, bool logging = true);
 		
 		/// Copy-constructor copies the state of a game but not the actual
 		/// players (the state of the board and knowledge who's turn it is
 		/// will be copied).
 		Game(const Game& game);
 		
+		
+		virtual ~Game();
+		
 		/// The game board.
 		/// \returns A reference to the game's board.
 		const Board& board() const;
 		
 		
+		/// Gets a copy of the log of moves.
+		/// \returns A vector log of the turns player. Each element of the
+		/// vector is a turn. Each turn is a vector of one or more moves.
+		/// If logging is not enabled an empty vector is returned.
+		std::vector< std::vector<Move> > log() const;
+		
+		
+		/// Get whether the game is being logged.
+		/// \returns \c true if logging is enabled, or \c false otherwise.
+		bool logging() const;
+		
+		
 	private:
+	
+		void logTurn(const std::vector<Move>& turn);
 		
 		/// The game's board.
 		Board state;
@@ -47,6 +69,10 @@ class zebra::Game
 		
 		/// Pointer to the player representing white.
 		Player* white;
+		
+		/// Optional log of the turns which took place.
+		std::vector< std::vector<Move> >* history;
+		
 };
 
 
